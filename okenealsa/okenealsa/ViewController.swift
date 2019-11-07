5429ab4f230fa24fb1ba65353126f1c6fceaf67c
松岡隆斗 <e185703@ie.u-ryukyu.ac.jp>
2019-11-07 11:36:02 +0900

    project directoryの追加

---
+++ okenealsa/okenealsa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  okenealsa
+//
+//  Created by 松岡　隆斗 on 2019/11/07.
+//  Copyright © 2019 松岡　隆斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
